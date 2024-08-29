d7830d8d0b8b3539e2ea8403929f49ccafbe4c3b
TokenW <347362000@qq.com>
2024-08-29 10:53:34 +0800

    Update run.sh

--- data/run.sh
+++ data/run.sh
@@ -2,10 +2,7 @@
 
 # 定义下载的文件列表
 urls=(
-  "https://raw.githubusercontent.com/qq5460168/dangchu/main/black.txt" #5460
-  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
-  "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt"  #DD
-  "https://raw.gitmirror.com/Cats-Team/dns-filter/main/abp.txt" #AdRules DNS Filter
+  "https://mirror.ghproxy.com/raw.githubusercontent.com/qq5460168/666/master/dns.txt/"
   "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" #https://github.com/StevenBlack/hosts
   "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts" #奖励形式广告、爱奇艺、小米视频、腾讯视频、搜狐视频等
   "https://raw.githubusercontent.com/tiuxo/hosts/master/ads"
@@ -13,13 +10,10 @@ urls=(
   "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt" #LWJ's black list
   "https://adguardteam.github.io/HostlistsRegistry/assets/filter_21.txt" #CHN: anti-AD
   "https://adguardteam.github.io/HostlistsRegistry/assets/filter_29.txt" #CHN: AdRules DNS List
-  "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt" #白名单
-  "https://raw.githubusercontent.com/loveqqzj/AdGuard/master/Mobile.txt" #loveqqzj
-  "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt" #mphin
-  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list" #周木木
-  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt" #liwenjie119
    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/doh-vpn-proxy-bypass.txt" #HaGeZi's Encrypted
   "https://adguardteam.github.io/HostlistsRegistry/assets/filter_23.txt" #WindowsSpyBlocker - Hosts spy rules
+
+
 )
 
 # 临时文件夹
